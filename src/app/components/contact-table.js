85bd820628ba07338fb17342ecb5c2d5edeed1f2
Elgara5364 <steveloardi5364@gmail.com>
2024-04-16 18:15:33 +0900

    add button component and use in contact table

--- src/app/components/contact-table.js
+++ src/app/components/contact-table.js
@@ -1,4 +1,6 @@
 import { getContacts } from "../lib/data";
+import { formatDate } from "../lib/utils";
+import { DeleteButton, EditButton } from "./button";
 
 const ContactTable = async () => {
   const contacts = await getContacts();
@@ -20,8 +22,13 @@ const ContactTable = async () => {
             <td className="py-3 px-6">{index + 1}</td>
             <td className="py-3 px-6">{contact.name}</td>
             <td className="py-3 px-6">{contact.phone}</td>
-            <td className="py-3 px-6">{contact.createdAt.toString()}</td>
-            <td></td>
+            <td className="py-3 px-6">
+              {formatDate(contact.createdAt.toString())}
+            </td>
+            <td className="flex space-x-1 justify-center py-3">
+              <EditButton />
+              <DeleteButton />
+            </td>
           </tr>
         ))}
       </tbody>
